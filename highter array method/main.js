12005a4b7a2d18cedf74ff89fbdbd16579700272
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-01-07 17:41:27 +0000

    add

---
+++ highter array method/main.js
@@ -0,0 +1,5 @@
+const fruit = ["apple","banana","suqaar"];
+
+fruit.forEach((fruit)=>{
+    console.log('i like ' + fruit);
+})
--- stament ofifand else/main.js
+++ stament ofifand else/main.js
@@ -9,17 +9,26 @@
 
 // let temperature = 20;
 
-temperature = prompt("enter the temperature ");
+// temperature = prompt("enter the temperature ");
 
-if (temperature <= 5){
-    console.log("temperature is very cold")
-}
-else if (temperature <= 15){
-  console.log("temperature is cold")
-}
-else if (temperature <= 25){
-  console.log("temperature is warm")
-}
-else{
-    console.log("temperature is hot")
-};
+// if (temperature <= 5){
+//     console.log("temperature is very cold")
+// }
+// else if (temperature <= 15){
+//   console.log("temperature is cold")
+// }
+// else if (temperature <= 25){
+//   console.log("temperature is warm")
+// }
+// else{
+//     console.log("temperature is hot")
+// };
+
+// ternany operator
+let age = 15;
+const massege = age >= 18 ? "your ADult" : "your are baby"
+
+console.log(massege)
+
+const me = age >= 12 && "your ah,ed"
+console.log(me);
