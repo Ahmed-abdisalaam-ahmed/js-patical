c47fdb6f61a63e25c93e4e8fe15a0c991d0b22a6
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-01-07 17:54:27 +0000

    adding array loop

--- highter array method/main.js
+++ highter array method/main.js
@@ -3,3 +3,32 @@ const fruit = ["apple","banana","suqaar"];
 fruit.forEach((fruit)=>{
     console.log('i like ' + fruit);
 })
+
+
+let student = [
+    {
+     id : 1,
+     name: "ahmed",
+     age :18,
+     grade:"9A"
+    },
+    {
+     id : 2,
+     name: "jamman jimcale",
+     age :20,
+     grade:"9b"
+    },
+    {
+     id : 3,
+     name: "ali said sonkor",
+     age :15,
+     grade:"8A"
+    }
+];
+student.forEach((student)=>{
+    // console.log(student)
+        console.log("---------------")
+        for(const key in student){
+        console.log(student[key])
+        }
+})
