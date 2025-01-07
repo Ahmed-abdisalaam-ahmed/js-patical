69543908ec5d1daf2060dc4e7693f15c6f71b489
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-01-07 22:49:24 +0000

    map and

--- highter array method/main.js
+++ highter array method/main.js
@@ -1,41 +1,51 @@
-// FOREach
+// // FOREach
 
 
-const fruit = ["apple","banana","suqaar"];
+// const fruit = ["apple","banana","suqaar"];
 
-fruit.forEach((fruit)=>{
-    console.log('i like ' + fruit);
-})
+// fruit.forEach((fruit)=>{
+//     console.log('i like ' + fruit);
+// })
 
 
-let student = [
-    {
-     id : 1,
-     name: "ahmed",
-     age :18,
-     grade:"9A"
-    },
-    {
-     id : 2,
-     name: "jamman jimcale",
-     age :20,
-     grade:"9b"
-    },
-    {
-     id : 3,
-     name: "ali said sonkor",
-     age :15,
-     grade:"8A"
-    }
-];
-student.forEach((student)=>{
-    // console.log(student)
-        console.log("---------------")
-        for(const key in student){
-        console.log(student[key])
-        }
-})
+// let student = [
+//     {
+//      id : 1,
+//      name: "ahmed",
+//      age :18,
+//      grade:"9A"
+//     },
+//     {
+//      id : 2,
+//      name: "jamman jimcale",
+//      age :20,
+//      grade:"9b"
+//     },
+//     {
+//      id : 3,
+//      name: "ali said sonkor",
+//      age :15,
+//      grade:"8A"
+//     }
+// ];
+// student.forEach((student)=>{
+//     // console.log(student)
+//         console.log("---------------")
+//         for(const key in student){
+//         console.log(student[key])
+//         }
+// })
 
-// Map
+// // Map
+
+// const number = [1,2,3,4,5,6,7];
+
+// const forNumber = number.map((number)=> number*2)
+// console.log(forNumber)
+
+
+const fruit = ["apple","banana","jerry"];
+const lenght = fruit.map((fruit)=>fruit +" "+ fruit.length)
+console.log("this fruits is "+ lenght)
 
 
