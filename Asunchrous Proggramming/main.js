03648de13a8188ee6c8313fe24d21f72f1fcc3bb
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-01-22 19:09:21 +0000

    finished

--- Asunchrous Proggramming/main.js
+++ Asunchrous Proggramming/main.js
@@ -16,15 +16,59 @@
 
 
 
-function getUserdata(callback){
-    setTimeout(() => {
-    const user = {id: 1 ,name: "john smith"};
-     callback(user)
-    }, 1000);
+// function getUserdata(callback){
+//     setTimeout(() => {
+//     const user = {id: 1 ,name: "john smith"};
+//      callback(user)
+//     }, 1000);
+// }
+// console.log("hello world")
+
+// getUserdata(function (user){
+//     console.log(user)
+// });
+
+
+// unblocking
+// function student (){
+//     alert("warfaa waad soo dahday, Dhakhso Ok taabo!")
+//     return {id :1, name: "mohammed", age:17}
+// }
+// console.log("the proccess is starting ........");
+
+// const list = student()
+// console.log("Now can you see the list",list);
+
+// console.log("finished");
+
+// block
+// function student(callback){
+//     setTimeout(() =>{
+//         const login = {id :1, name: "mohammed", age:17}
+//         callback(login)
+//     },3000);
+// };
+// console.log("the proccess is starting ........");
+
+// student(function(user){
+//     console.log("Now can you see the list",user);
+// })
+
+// console.log("finished");
+
+function fetchsylData (){
+    return new Promise ((Resolve , Reject) => {
+        setTimeout(() => {
+            const Secceses = false;
+            if (Secceses){
+                Resolve({id: 1 , name:"ahmed shehab"})
+            }
+            else{
+                Reject("the fetch data is failed")
+            }
+        },3000)
+    })
 }
-console.log("hello world")
-
-getUserdata(function (user){
-    console.log(user)
-});
-
+fetchsylData()
+    .then((data)=> console.log("User Data" , data))
+    .catch((err)=> console.log("Error", err))
