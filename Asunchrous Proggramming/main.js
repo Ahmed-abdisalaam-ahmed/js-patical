e9b035b31774c0895c0bc77da36ecea389546d01
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-02-05 13:02:02 +0000

    finished asynochronous

--- Asunchrous Proggramming/main.js
+++ Asunchrous Proggramming/main.js
@@ -56,19 +56,166 @@
 
 // console.log("finished");
 
-function fetchsylData (){
-    return new Promise ((Resolve , Reject) => {
-        setTimeout(() => {
-            const Secceses = false;
-            if (Secceses){
-                Resolve({id: 1 , name:"ahmed shehab"})
-            }
-            else{
-                Reject("the fetch data is failed")
-            }
-        },3000)
+// function fetchsylData (){
+//     return new Promise ((Resolve , Reject) => {
+//         setTimeout(() => {
+//             const Secceses = false;
+//             if (Secceses){
+//                 Resolve({id: 1 , name:"ahmed shehab"})
+//             }
+//             else{
+//                 Reject("the fetch data is failed")
+//             }
+//         },3000)
+//     })
+// }
+// fetchsylData()
+//     .then((data)=> console.log("User Data" , data))
+//     .catch((err)=> console.log("Error", err))
+
+// async function DisplayDatafetch() {
+//     try{
+//         const user = await(fetchsylData())
+//         console.log(user)
+//     }
+//    catch(err){
+//     console.log("Error",err)
+//    }
+// }
+// DisplayDatafetch()
+
+
+// function usercheck(){
+//     return new Promise((Resolve , Reject)=>{
+//         setTimeout(()=>{
+//             const password = 0
+//         },3000)
+//     })
+// }
+
+// JSON 
+
+// Json to object
+// object to json
+
+// let Student = {
+//     id:1,
+//     name:"ahmed",
+//     age:"12"
+// }
+
+// const jsonstring = JSON.stringify(Student) 
+// console.log(jsonstring);
+
+// const ChangeJson = JSON.parse(jsonstring)
+
+// console.log(ChangeJson)
+
+
+
+// fetch json and Api
+// async function fetchJson(){
+//     console.log("the fetching is starting.........");
+    
+//     const Reponse = await fetch('data.json');
+
+//     const data = await Reponse.json()
+
+//     // const changejson = JSON.stringify(data)
+    
+//     console.log("Respons", data)
+// }
+
+// fetchJson()
+
+// function opertion(a, b, callback){
+//     return callback(a,b)
+// }
+
+// function addtion(a , b){
+//     return a + b
+// }
+
+// function Subtration(a , b){
+//     return a - b
+// }
+// console.log("addition", opertion(12 ,22,addtion));
+// console.log("Subtraction", opertion(12 ,22,Subtration));
+
+// function greet(callback){
+//     console.log("HELLO, ", name)
+// }
+
+// function proceesUser(){
+    
+// }
+
+
+// HTTP REQUAST 
+
+// Get 
+// Post 
+// Upadate
+// delete
+
+// GET
+
+// async function fetchData() {
+//     try {
+//         console.log("the fetch Data is Starting .........")
+
+//         const respone = await fetch('https://jsonplaceholder.typicode.com/posthhs/1')
+
+//         // Get HTTP request
+//         if(!respone.ok){
+//           throw new Error (`HTTP Status error !Status ${respone.status}`)
+//         }
+//         const data = await respone.json();
+
+//         console.log(data)
+
+//     }
+//     catch(err){
+//       console.log(err)
+//     }
+// }
+// fetchData()
+
+// Post
+
+async function postdata(){
+
+  try{
+    const respone = await fetch ('https://jsonplaceholder.typicode.com/posts',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        title:"what is this in title ",
+        body: "this body ar in go to post",
+        userId: 1
+      })
     })
+
+if (!respone.ok){
+  throw new error (`HTTP ERROR !Status${Response.status}`)
 }
-fetchsylData()
-    .then((data)=> console.log("User Data" , data))
-    .catch((err)=> console.log("Error", err))
+
+//
+  console.log('before   ',respone)
+console.log("------------------------------")
+console.log("------------------------------")
+console.log("------------------------------")
+console.log("------------------------------")
+console.log("------------------------------")
+console.log("------------------------------")
+const data = await respone.json()
+
+  console.log('after   ',data)
+}
+  catch(error){
+    console.log(error)
+  }
+}
+postdata()
